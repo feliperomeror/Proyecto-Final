51b8f43df1dd524eff68baef3dfbe4e73e9dccb0
feliperomeror <romero-af@javeriana.edu.co>
2026-06-02 23:11:47 -0500

    Update Proyecto_final_mundial_2026.R

--- scripts/Proyecto_final_mundial_2026.R
+++ scripts/Proyecto_final_mundial_2026.R
@@ -1,1 +1,56 @@
+# =========================================================
+# PROYECTO FINAL - ANALÍTICA DE NEGOCIOS
+# MODELO PREDICTIVO POTENCIA MUNDIALISTA 2026
+# =========================================================
+
+# =========================================================
+# LIBRERÍAS
+# =========================================================
+
+library(readxl)
+library(dplyr)
+library(pscl)
+library(car)
+library(stargazer)
+
+# =========================================================
+# CARGAR BASE
+# =========================================================
+
+data <- read_excel(
+  "Base_Mundial_2026_COMPLETA.xlsx"
+)
+
+# =========================================================
+# LIMPIAR NOMBRES
+# =========================================================
+
+library(janitor)
+
+data <- clean_names(data)
+
+# =========================================================
+# REVISAR ESTRUCTURA
+# =========================================================
+
+str(data)
+summary(data)
+
+# =========================================================
+# MODELO LOGÍSTICO FINAL
+# =========================================================
+
+modelo_logit <- glm(
+  
+  potencia_mundial ~
+    
+    elo_rating +
+    ranking_fifa +
+    valor_plantel_m,
+  
+  family = binomial(link = "logit"),
+  
+  data = data
+  
+)
 
